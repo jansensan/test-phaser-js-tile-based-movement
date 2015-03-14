29c692421c0dfc228eb7428ef7b725f106af3a16
Mat Janson Blanchet <mat.jb@jansensan.net>
2015-03-14 21:20:41 +0100

    Manually set collision, also added allowed get methods

--- src/assets/js/src/map.js
+++ src/assets/js/src/map.js
@@ -1,6 +1,9 @@
 function Map(game) {
   // constants
   var MAP_NAME = 'Map',
+      TILE_SIZE = 16,
+      NUM_COLUMNS = 15,
+      NUM_ROWS = 10,
       Tilesets = {
         TILES: 'FF4 Basic Tileset',
         COLLISION: 'Collision Tileset'
@@ -28,7 +31,8 @@ function Map(game) {
   _class.preload = preload;
   _class.init = init;
   _class.getTilesLayer = getTilesLayer;
-  _class.getCollisionLayer = getCollisionLayer;
+  _class.getCollisionAt = getCollisionAt;
+  _class.getSurroundingCollisionsAt = getSurroundingCollisionsAt;
 
 
   // private methods
@@ -43,7 +47,6 @@ function Map(game) {
     _game.load.image(Tilesets.COLLISION, AssetsPaths.COLLISION);
   }
 
-
   function init() {
     // create a map with the map name given in tile editor
     _map = _game.add.tilemap(MAP_NAME);
@@ -57,22 +60,102 @@ function Map(game) {
     _collisionLayer = _map.createLayer(1);
     _collisionLayer.alpha = 0;
 
-    // set basic collision
-    _map.setCollisionByExclusion([0, 1]);
+    // manually set directional collision
+    var i = 0, j = 0,
+        collisionTile,
+        targetTile,
+        hasCollision;
+    for(i = 0; i < NUM_COLUMNS; i++) {
+      for(j = 0; j < NUM_ROWS; j++) {
+        // get collision data from collision layer
+        collisionTile = _collisionLayer.layer.data[j][i];
+        hasCollision = (collisionTile.index > -1);
+
+        // set collision uniformely
+        // (no cloud tile for now)
+        targetTile = _tilesLayer.layer.data[j][i];
+        targetTile.collideDown = hasCollision;
+        targetTile.collideLeft = hasCollision;
+        targetTile.collideRight = hasCollision;
+        targetTile.collideUp = hasCollision;
+      }
+    }
 
     _tilesLayer.resizeWorld();
     _collisionLayer.resizeWorld();
   }
 
-
   function getTilesLayer() {
     return _tilesLayer;
   }
 
-
   function getCollisionLayer() {
     return _collisionLayer;
   }
+
+  function getCollisionAt(tile) {
+    var tileData = _tilesLayer.layer.data[tile.y][tile.x];
+    var hasCollision = (tileData.collideUp && tileData.collideRight && tileData.collideDown && tileData.collideLeft);
+    // console.log('hasCollision: ', hasCollision);
+    return hasCollision;
+  }
+
+  function getSurroundingCollisionsAt(tile) {
+    var surroundings = {
+      up: false,
+      right: false,
+      down: false,
+      left: false
+    };
+
+    // check tile up
+    var tileUp = {
+      x: tile.x,
+      y: tile.y - 1
+    };
+    if(tileUp.y < 0) {
+      surroundings.up = true;
+    } else {
+      surroundings.up = getCollisionAt(tileUp);
+    }
+
+    // check tile right
+    var tileRight = {
+      x: tile.x + 1,
+      y: tile.y
+    };
+    if(tileRight.x >= NUM_COLUMNS) {
+      surroundings.right = true;
+    } else {
+      surroundings.right = getCollisionAt(tileRight);
+    }
+
+    // check tile down
+    var tileDown = {
+      x: tile.x,
+      y: tile.y + 1
+    };
+    if(tileDown.y >= NUM_ROWS) {
+      surroundings.down = true;
+    } else {
+      surroundings.down = getCollisionAt(tileDown);
+    }
+
+    // check tile left
+    var tileLeft = {
+      x: tile.x - 1,
+      y: tile.y
+    };
+    if(tileLeft.x < 0) {
+      surroundings.left = true;
+    } else {
+      surroundings.left = getCollisionAt(tileLeft);
+    }
+
+    return surroundings;
+  }
+
+
 
 
   return _class;
