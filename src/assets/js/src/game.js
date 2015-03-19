83e3cecc8950d99ae24a2c50549cb3fb96e174df
Mat Janson Blanchet <mat.jb@jansensan.net>
2015-03-20 00:28:40 +0100

    Added device layout

--- src/assets/js/src/game.js
+++ src/assets/js/src/game.js
@@ -14,7 +14,13 @@
       _player = null,
       _tonberry = null,
 
-      // reference for code reduction
+      // controls
+      _dpad = {
+        up: null,
+        right: null,
+        down: null,
+        left: null
+      },
       _keyboardInput = null;
 
 
@@ -42,6 +48,13 @@
     // create player
     _player = new Player(_game, _map);
     _tonberry = new NPC(_game, _map);
+  }
+
+  function getDOMButtons() {
+    _dpad.up = document.getElementById('dpad-up');
+    _dpad.right = document.getElementById('dpad-right');
+    _dpad.down = document.getElementById('dpad-down');
+    _dpad.left = document.getElementById('dpad-left');
   }
 
   function preload() {
